e21343d80f335fcc471f6852f1733c97fb277690
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-11-11 05:54:19 +0000

    - Updated dependency on Date::Gregorian::Simple v0.08.

--- lib/Calendar/Gregorian.pm
+++ lib/Calendar/Gregorian.pm
@@ -1,6 +1,6 @@
 package Calendar::Gregorian;
 
-$Calendar::Gregorian::VERSION   = '0.09';
+$Calendar::Gregorian::VERSION   = '0.10';
 $Calendar::Gregorian::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Calendar::Gregorian - Interface to Gregorian Calendar.
 
 =head1 VERSION
 
-Version 0.09
+Version 0.10
 
 =cut
 
