90a98dd502a5cb4a4c632f0722fb6c426fad355e
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-12-05 12:50:54 +0000

    - Added test scripts (t/meta-json.t and t/meta-yml.t).

--- lib/Calendar/Gregorian.pm
+++ lib/Calendar/Gregorian.pm
@@ -1,6 +1,6 @@
 package Calendar::Gregorian;
 
-$Calendar::Gregorian::VERSION   = '0.10';
+$Calendar::Gregorian::VERSION   = '0.11';
 $Calendar::Gregorian::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Calendar::Gregorian - Interface to Gregorian Calendar.
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
